e54d8ca84c4c76dffb6131008c0f8ec6d6ff3b25
Kyle Yu <ninetailsbankai@gmail.com>
2014-06-04 14:33:45 -0400

    add gamescreen

---
+++ core/src/com/pewgame/screen/GameScreen.java
@@ -0,0 +1,17 @@
+package com.pewgame.screen;
+
+public class GameScreen extends PewScreen {
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		super.show();
+	}
+	
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		super.render(delta);
+
+	}
+}
