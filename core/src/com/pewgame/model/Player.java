4ba8de2db5fc83aea4b501aefdd11a73a17e0713
Kyle Yu <ninetailsbankai@gmail.com>
2014-06-04 15:51:08 -0400

    add ball

---
+++ core/src/com/pewgame/model/Player.java
@@ -0,0 +1,60 @@
+package com.pewgame.model;
+
+import com.badlogic.gdx.math.Circle;
+
+public class Player {
+	private float x, y;
+	private int radius;
+	private float velX = 0, velY = -10;
+	Circle circ;
+	
+	public Player(float x, float y, int radius) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		circ = new Circle(x, y, radius);
+	}
+	
+	public void update(float delta) {
+		x += velX * delta;
+		y += velY * delta;
+		
+		if (y < 1) {
+			y = 1;
+			velY = -velY;
+		}
+		
+		velY -= 2;
+		
+		circ.setPosition(x, y);
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	public Circle getCirc() {
+		return circ;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+}
--- core/src/com/pewgame/model/Tile.java
+++ core/src/com/pewgame/model/Tile.java
@@ -1,5 +1,7 @@
 package com.pewgame.model;
 
+import com.badlogic.gdx.math.Rectangle;
+
 public class Tile {
 
 	public enum TileType {
@@ -8,17 +10,59 @@ public class Tile {
 
 	private float x, y;
 	private int width, height;
-	private int type;
+	private TileType type;
+	private Rectangle rect;
 
-	public Tile(float x, float y, int width, int height, int type) {
+	public Tile(float x, float y, int width, int height, TileType type) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 		this.type = type;
+		rect = new Rectangle(x, y, width, height);
 	}
 
 	public void update(float delta) {
 	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public TileType getType() {
+		return type;
+	}
+
+	public void setType(TileType type) {
+		this.type = type;
+	}	
 
 }
