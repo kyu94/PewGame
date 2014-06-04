cf74028b22ba0796dbb36f240bcf546edb71b9b2
James Cho <jamescho7@gmail.com>
2014-06-04 14:45:45 -0400

    Implemented MenuScreen

--- core/src/com/pewgame/screen/GameScreen.java
+++ core/src/com/pewgame/screen/GameScreen.java
@@ -1,7 +1,12 @@
 package com.pewgame.screen;
 
+import com.mygdx.game.PewGame;
+
 public class GameScreen extends PewScreen {
 	
+	public GameScreen(PewGame game) {
+	}
+
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
